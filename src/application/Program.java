ac283331d985517dd6aa7f5538808464ebe49f54
Francisco Aguiar <franciscosouzalima@gmail.com>
2022-05-13 15:11:50 -0300

    inclusao de candidatos dinâmica e contagem dinâmica

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,38 @@ public class Program {
 		
 		Boolean votacaoRolando = true;
 		Urna urna;
-		int voto;
+		int numeroCandidatos;
+		int numeroCandidato;
 		
 		urna = new Urna();
-		urna.iniciarEleicao();
+		
+		urna.iniciarUrna();
+		numeroCandidatos = sc.nextInt();
+		urna.numeroCandidatos(numeroCandidatos);
+		
+		for(int i = 0; i < numeroCandidatos; i += 1) {
+			String nomeCandidato;
+			
+			System.out.println("Informe o nome do candidato nº" + (i+1) + ": ");
+			nomeCandidato = sc.next();
+			
+			System.out.println("Informe o número do candidato " + nomeCandidato + ": ");
+			numeroCandidato = sc.nextInt();
+			
+			urna.incluirCandidato(i, numeroCandidato + " - " + nomeCandidato);
+			
+			if (i == numeroCandidatos-1) {
+				System.out.println("\n[****** Candidatos Cadastrados: ******]\n");
+				urna.imprimirCandidatos();	
+			}
+		}
 		
 		while (votacaoRolando) {
-			urna.mostrarCandidatos();
+			int voto;
 			
+			urna.mostrarCandidatos();
 			voto = sc.nextInt();
+			
 			
 			if (voto == 0) {
 				votacaoRolando = false;
@@ -26,6 +49,7 @@ public class Program {
 				urna.computarVoto(voto);
 			}
 		}
+		
 		urna.resultadoEleicao();
 	}
 }
--- src/entities/Urna.java
+++ src/entities/Urna.java
@@ -3,35 +3,60 @@ package entities;
 
 public class Urna {
 	
-	private int votosLula;
-	private int votosBozo;
+	private String[] candidatos;
+	private int[] votosCandidatos;
+	int totalVotos = 0;
+	
+	
+	public void iniciarUrna() {
+		System.out.println("\n[****** Inicializando a Urna ******]\n");
+		System.out.println("Quantos candidatos vão participar da eleição?");
+	}
+	
+	public void numeroCandidatos(int numeroCandidatos) {
+		candidatos = new String[numeroCandidatos];
+		votosCandidatos = new int[numeroCandidatos];
+		for(int i = 0; i < votosCandidatos.length; i+=1) {
+			votosCandidatos[i] = 0;
+		}
+	}
+	
+	public void incluirCandidato(int posicao, String nomeCandidato) {
+		candidatos[posicao] = nomeCandidato;
+	}
+	
+	public void imprimirCandidatos() {
+		for(int i = 0; i < candidatos.length; i+=1) {
+			System.out.println(candidatos[i]);
+		}
+	}	
 	
 	public void iniciarEleicao() {
-		System.out.println("\n[****** Eleições Iniciadas - Escolha seu Candidato ******]\n");
+		System.out.println("\n[****** Eleições Iniciadas ******]\n");
+		
 	}
 	
 	public void mostrarCandidatos() {
-		System.out.println("Digite o numero do seu candidato ou [0] para finalizar a eleição: ");
-		System.out.println("PT - Lula - 13");
-		System.out.println("PL - Bolsonaro - 17");
+		System.out.println("\n[****** Escolha seu Candidato ******]\n");
+		for(int i = 0; i < candidatos.length; i+=1) {
+			System.out.println(candidatos[i]);
+		}
 	}
 	
 	public void computarVoto(int voto) {
-		if (voto == 13) {
-			votosLula += 1;
-		} else if (voto == 17) {
-			votosBozo += 1;
+		for (int i = 0; i < candidatos.length; i+=1) {
+			if(voto == Integer.parseInt(candidatos[i].substring(0,2))) {
+				votosCandidatos[i] += 1;
+				totalVotos += 1;
+			}
 		}
 	}
 	
 	public void resultadoEleicao() {
 		System.out.println("\n[***************** Resultado da Eleição *****************]");
-		System.out.println("PT - Lula - Qtd de Votos: " + votosLula + " - " + 100*votosLula/(votosLula+votosBozo) + "%");
-		System.out.println("PL - Bolsonaro - Qtd de Votos: " + votosBozo + " - " + 100*votosBozo/(votosLula+votosBozo) + "%");
-		if (votosLula>votosBozo) {
-			System.out.println("\n[***************** Vencedor - LULA  *****************]");
-		} else {
-			System.out.println("\n[***************** Vencedor - BOLSONARO  *****************]");
-		}	
+		for(int i = 0; i < candidatos.length; i+=1) {
+			System.out.print(candidatos[i].substring(5) + " (" + candidatos[i].substring(0,2) + ") - ");
+			System.out.println("Total de votos: " + votosCandidatos[i] + " | " + votosCandidatos[i]*100/totalVotos + "%");
+		}
 	}
 }
